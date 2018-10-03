f97107c012a67921407deee681e876441c9c8f0b
Dick Ittmann <dick.ittmann@reports.nl>
2018-10-03 15:24:13 +0200

    Added TeamGroup service

---
+++ module/DbAPI/src/V1/Rest/TeamGroup/TeamGroupCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace DbAPI\V1\Rest\TeamGroup;
+
+use Zend\Paginator\Paginator;
+
+class TeamGroupCollection extends Paginator
+{
+}
---
+++ module/DbAPI/src/V1/Rest/TeamGroup/TeamGroupResource.php
@@ -0,0 +1,8 @@
+<?php
+namespace DbAPI\V1\Rest\TeamGroup;
+
+use ZF\Apigility\Doctrine\Server\Resource\DoctrineResource;
+
+class TeamGroupResource extends DoctrineResource
+{
+}
